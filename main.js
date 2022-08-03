ddd4efe26fecdd06f1f669f1eaa32f8fb7bc9694
Muhammadkadhim <muhammad.kadhim.hassan@gmail.com>
2022-08-03 22:35:14 +0300

    Advice Generator App

---
+++ main.js
@@ -0,0 +1,22 @@
+let adviceGenerateBtn = document.getElementById("adviceGenerateBtn");
+let adviceContainer = document.getElementById("adviceContainer");
+let adviceNumberContainer = document.getElementById("adviceNumberContainer");
+
+let adviceNumber = 1;
+
+function fetchAdvice() {
+    adviceNumberContainer.innerText = "#" + adviceNumber;
+
+    fetch("https://api.adviceslip.com/advice")
+        .then((res) => res.json())
+        .then((data) => (adviceContainer.innerText = data.slip.advice))
+        .catch((err) => console.log(err));
+}
+// onload
+fetchAdvice();
+
+// on click
+adviceGenerateBtn.addEventListener("click", () => {
+    adviceNumber = adviceNumber + 1;
+    fetchAdvice();
+});
